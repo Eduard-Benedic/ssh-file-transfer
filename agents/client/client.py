5dba8b968ea4d88bea96c691d1de3b3f5796a76c
Eduard Benedic <benediceduard@outlook.com>
2022-08-12 21:25:21 +0100

    Add documentation

--- agents/client/client.py
+++ agents/client/client.py
@@ -1,19 +1,36 @@
-
 import socket
 from paramiko import SSHClient, AutoAddPolicy
 import signal
 signal.signal(signal.SIGINT, signal.SIG_DFL)
 
-
-def send_file(file: bytes, file_path: str):
+"""
+=========================================================
+Name        :  client.py
+Author      :  Eduard Benedic
+Description :  SSH client
+Sources     :
+              {1} - https://docs.paramiko.org/en/stable/api/client.html
+              {2} - https://stackoverflow.com/questions/3878082/python-paramiko
+=========================================================
+"""
+def send_file(file: bytes, file_extension: str):
+  # create SSHClient {1}, a wrapper arround the Transport, Channel and SFTP client
   client = SSHClient()
 
+  # if the client doesn't recognize the server, initialize the connection anyway {2}
   client.set_missing_host_key_policy(AutoAddPolicy())
 
-  client.connect(hostname='127.0.0.1', port=2200, password="foo", username="robey")
+  # connect to the server
+  client.connect(
+    hostname='127.0.0.1',
+    port=2200,
+    password="foo",
+    username="robey"
+  )
+
   sftp = client.open_sftp()
 
-  sftp.putfo(file, file_path)
+  sftp.putfo(file, file_extension)
 
   sftp.close()
 
